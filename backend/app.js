e9bbf045d39f6278d351cee5d3a270ddc14b8e73
geramirez03 <gabrieler2000@hotmail.com>
2023-10-23 13:06:53 -0700

    Create Node Express backend API

---
+++ backend/app.js
@@ -0,0 +1,34 @@
+/** Express app for MyGamingList */
+
+const express = require('express');
+
+const { NotFoundError } = require("./expressError");
+
+// const gameRoutes = require("./routes/games");
+// const userRoutes = require("./routes/users");
+
+const app = express();
+
+app.use(express.json());
+
+// app.use("/games", gameRoutes);
+// app.use("/users", userRoutes);
+
+/** Handle 404 errors */
+app.use(function (req, res, next) {
+    return next(new NotFoundError());
+});
+
+/** Generic error handler */
+app.use(function (err, req, res, next) {
+    if (process.env.NODE_ENV !== "test") console.error(err.stack);
+
+    const status = err.status || 500;
+    const message = err.message;
+
+    return res.status(status).json({
+        error: { message, status },
+    });
+});
+
+module.exports = app;
---
+++ backend/config.js
@@ -0,0 +1,32 @@
+/** Shared config for MyGamingList */
+
+require("dotenv").config();
+require("colors");
+
+const SECRET_KEY = process.env.SECRET_KEY || "mgl-secret-dev";
+
+const PORT = +process.env.PORT || 3001;
+
+// Use dev database, testing database, or via env var, production database
+function getDatabaseUri() {
+  return (process.env.NODE_ENV === "test")
+      ? "postgresql:///my_gaming_list_test"
+      : process.env.DATABASE_URL || "postgresql:///my_gaming_list";
+}
+
+// Speed up bcrypt during tests, since the algorithm safety isn't being tested
+const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 13;
+
+console.log("Jobly Config:".green);
+console.log("SECRET_KEY:".yellow, SECRET_KEY);
+console.log("PORT:".yellow, PORT.toString());
+console.log("BCRYPT_WORK_FACTOR".yellow, BCRYPT_WORK_FACTOR);
+console.log("Database:".yellow, getDatabaseUri());
+console.log("---");
+
+module.exports = {
+  SECRET_KEY,
+  PORT,
+  BCRYPT_WORK_FACTOR,
+  getDatabaseUri,
+};
