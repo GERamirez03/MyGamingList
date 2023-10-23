8f0f5cf34389aef081593b1a78303208e86ddf7e
geramirez03 <gabrieler2000@hotmail.com>
2023-10-23 12:52:56 -0700

    Create SQL Seed file for development

---
+++ backend/seed.sql
@@ -0,0 +1,23 @@
+-- my_gaming_list
+-- my_gaming_list_test
+
+DROP DATABASE IF EXISTS my_gaming_list;
+
+CREATE DATABASE my_gaming_list;
+
+\c my_gaming_list
+
+CREATE TABLE users
+(
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE NOT NULL,
+    -- password_hash TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL
+);
+
+INSERT INTO users
+    (username, email)
+VALUES
+    ('testUser', 'testUser@test.com'),
+    ('testDev', 'testDev@test.com'),
+    ('testAdmin', 'testAdmin@test.com');
