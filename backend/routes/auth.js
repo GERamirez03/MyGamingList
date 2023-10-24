7f060455acf77616c1073e78d8123ca323e13672
geramirez03 <gabrieler2000@hotmail.com>
2023-10-24 09:18:56 -0700

    Implement basic login route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,6 +7,7 @@ const db = require("../db");
 const bcrypt = require("bcrypt");
 
 const { BCRYPT_WORK_FACTOR } = require("../config");
+const { ExpressError } = require("../expressError");
 
 /** Register user.
  *      {username, password, email} => {username}
@@ -23,6 +24,29 @@ router.post("/register", async function (req, res, next) {
             [username, hashedPassword, email]);
 
         return res.json(result.rows[0]);
+    } catch (err) {
+        return next(err);
+    }
+});
+
+/** Login user.
+ *      {username, password} => {message} on success
+ */
+
+router.post("/login", async function (req, res, next) {
+    try {
+        const { username, password } = req.body;
+        const result = await db.query(`
+            SELECT password FROM users WHERE username = $1`,
+            [username]);
+        const user = result.rows[0];
+
+        if (user) {
+            if (await bcrypt.compare(password, user.password) === true) {
+                return res.json({ message: "Logged in!" });
+            }
+        }
+        throw new ExpressError("Invalid username/password", 400);
     } catch (err) {
         return next(err);
     }
