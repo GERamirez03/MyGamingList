d1c754c6eb2cf5b7ebe2f42db9904f1340ca742a
geramirez03 <gabrieler2000@hotmail.com>
2023-10-25 12:49:57 -0700

    Implement '/users/:username' route for user details for admin and target user only

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,7 +4,7 @@ const router = new express.Router();
 
 const db = require("../db");
 
-const { ensureAdmin } = require("../middleware/auth");
+const { ensureAdmin, ensureAdminOrTargetUser } = require("../middleware/auth");
 
 /** GET users: [user, user, user] */
 
@@ -14,6 +14,17 @@ router.get("/all", ensureAdmin, async function(req, res, next) {
         return res.json(results.rows);
     }
     catch (err) {
+        return next(err);
+    }
+});
+
+/** GET information on specified user */
+
+router.get("/:username", ensureAdminOrTargetUser, async function(req, res, next) {
+    try {
+        const results = await db.query(`SELECT id, username, email, is_admin FROM users WHERE username = $1`, [req.params.username]);
+        return res.json(results.rows[0]);
+    } catch (err) {
         return next(err);
     }
 });
