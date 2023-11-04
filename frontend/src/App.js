dff657a6ce0f732f4d22f556d62a7496e39d2e5b
geramirez03 <gabrieler2000@hotmail.com>
2023-11-04 14:31:51 -0700

    Update App.js deps

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
-import BasicButtons from './BasicButtons';
-import SignupForm from './SignupForm';
 import { BrowserRouter } from 'react-router-dom';
 
 function App() {
@@ -9,8 +7,6 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <CssBaseline />
-        <BasicButtons/> 
-        <SignupForm />
       </BrowserRouter>
     </div>
   );
