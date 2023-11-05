f25380926571bed1bbd86d93822572d8cbe364b5
geramirez03 <gabrieler2000@hotmail.com>
2023-11-05 11:30:13 -0800

    Install and use CORS for local testing

--- backend/app.js
+++ backend/app.js
@@ -1,30 +1,22 @@
 /** Express app for MyGamingList */
 
 const express = require('express');
-
-// import external middleware 'morgan' for logging
+const cors = require('cors');
 const morgan = require("morgan");
 
 const { NotFoundError } = require("./expressError");
+const { authenticateJWT } = require("./middleware/auth");
 
-// const gameRoutes = require("./routes/games");
 const userRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
 
-const { authenticateJWT } = require("./middleware/auth");
-
 const app = express();
 
-// parse JSON requests
+app.use(cors());
 app.use(express.json());
-
-// use morgan logger
 app.use(morgan('dev'));
-
-// attempt to authenticate JSON Web Token on every request received
 app.use(authenticateJWT);
 
-// app.use("/games", gameRoutes);
 app.use("/users", userRoutes);
 app.use("/auth", authRoutes);
 
