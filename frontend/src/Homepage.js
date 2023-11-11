783368e08f91be98e50584d0bf0ecc16e0ea901f
geramirez03 <gabrieler2000@hotmail.com>
2023-11-10 20:51:06 -0800

    Add user information to homepage

--- frontend/src/Homepage.js
+++ frontend/src/Homepage.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 function Homepage() {
+
+    const username = useSelector(store => store.username);
+    const token = useSelector(store => store.token);
+
     return (
         <div>
             <b>Welcome!</b>
+
+            {username && <p>Signed in as: { username }</p>}
+            {token && <p>with token: { token }</p>}
         </div>
     );
 }
