6e4ec3be1725df81576700bc238ca895f38953ab
geramirez03 <gabrieler2000@hotmail.com>
2023-11-22 10:16:51 -0800

    Refactor Homepage.js to use MUI Box and Typography components

--- frontend/src/Homepage.js
+++ frontend/src/Homepage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Box, Typography } from "@mui/material"
 
 function Homepage() {
 
@@ -7,12 +8,18 @@ function Homepage() {
     const token = useSelector(store => store.token);
 
     return (
-        <div>
-            <b>Welcome!</b>
-
-            {username && <p>Signed in as: { username }</p>}
-            {token && <p>with token: { token }</p>}
-        </div>
+        <Box sx={{ flexGrow: 1 }}>
+            <Typography variant="h1" component="div" sx={{ flexGrow: 1}}>
+                Welcome to MyGamingList!
+            </Typography>
+        {
+            username && token &&
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1}}>
+                Signed in as: { username }
+                With token: { token }
+            </Typography>
+        }
+        </Box>
     );
 }
 
