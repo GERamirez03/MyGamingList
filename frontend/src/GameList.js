2eceb4197d375333ac9c6e6af1e14839fe94d84a
geramirez03 <gabrieler2000@hotmail.com>
2023-12-06 10:24:31 -0800

    Implement GameList component for searching and displaying games

---
+++ frontend/src/GameList.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import MyGamingListApi from "./api";
+import SearchForm from "./SearchForm";
+import GameCard from "./GameCard";
+
+function GameList() {
+
+    const [searchFormData, setSearchFormData] = useState(null);
+    const [gamesArr, setGamesArr] = useState(null);
+
+    const search = searchFormData => {
+        setSearchFormData(searchFormData);
+    };
+
+    useEffect(() => {
+        async function getGameData(searchFormData) {
+            let apiHelper = new MyGamingListApi();
+            let games = await apiHelper.searchGames(searchFormData);
+            setGamesArr(games);
+        }
+        getGameData(searchFormData);
+    }, [searchFormData])
+
+    return (
+        <>
+            <SearchForm search={search} />
+            {gamesArr !== null && gamesArr.map(game => <GameCard game={game} />)}
+        </>
+    );
+}
+
+export default GameList;
