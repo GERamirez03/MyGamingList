8532be5441bda7de51abc804aa5fadc45ddc57cd
geramirez03 <gabrieler2000@hotmail.com>
2023-12-07 10:19:01 -0800

    Remove search field from Navbar for a cleaner interface

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -54,46 +54,6 @@ function Navbar() {
 
     const username = useSelector(store => store.username);
 
-    const Search = styled('div')(({ theme }) => ({
-        position: 'relative',
-        borderRadius: theme.shape.borderRadius,
-        backgroundColor: alpha(theme.palette.common.white, 0.15),
-        '&:hover': {
-          backgroundColor: alpha(theme.palette.common.white, 0.25),
-        },
-        marginRight: theme.spacing(2),
-        marginLeft: 0,
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-          marginLeft: theme.spacing(3),
-          width: 'auto',
-        },
-      }));
-      
-      const SearchIconWrapper = styled('div')(({ theme }) => ({
-        padding: theme.spacing(0, 2),
-        height: '100%',
-        position: 'absolute',
-        pointerEvents: 'none',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }));
-      
-      const StyledInputBase = styled(InputBase)(({ theme }) => ({
-        color: 'inherit',
-        '& .MuiInputBase-input': {
-          padding: theme.spacing(1, 1, 1, 0),
-          // vertical padding + font size from searchIcon
-          paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-          transition: theme.transitions.create('width'),
-          width: '100%',
-          [theme.breakpoints.up('md')]: {
-            width: '20ch',
-          },
-        },
-      }));
-
     return (<>
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
@@ -112,16 +72,6 @@ function Navbar() {
                     <Typography noWrap variant="h6" component="div" sx={{ display: { xs: 'none', sm: 'block' }, flexGrow: 1 }}>
                         MyGamingList
                     </Typography>
-
-                    <Search >
-                        <SearchIconWrapper >
-                            <SearchIcon />
-                        </SearchIconWrapper>
-                        <StyledInputBase
-                            placeholder="Search games…"
-                            inputProps={{ 'aria-label': 'search games' }}
-                        />
-                    </Search>
 
                     {!username && 
                     <>
