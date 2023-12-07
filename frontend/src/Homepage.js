96b481751c2f778f82dc32a29b852b9a55596713
geramirez03 <gabrieler2000@hotmail.com>
2023-12-07 10:26:47 -0800

    Add button in Homepage to take user to '/games/search' endpoint

--- frontend/src/Homepage.js
+++ frontend/src/Homepage.js
@@ -1,12 +1,23 @@
 import React from "react";
-import { Box, Typography } from "@mui/material"
+import { Box, Typography, Button } from "@mui/material"
+import { useNavigate } from "react-router-dom";
 
 function Homepage() {
+
+    const navigate = useNavigate();
+
+    const toSearch = () => {
+        navigate('/games/search');
+    };
+    
     return (
         <Box sx={{ flexGrow: 1 }}>
             <Typography variant="h1" component="div" sx={{ flexGrow: 1}}>
                 Welcome to MyGamingList!
             </Typography>
+            <Button variant="contained" onClick={toSearch}>
+                Search Games
+            </Button>
         </Box>
     );
 }
