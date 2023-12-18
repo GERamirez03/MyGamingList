45c630151aec7d1e43a26912161fb77fa01c1e11
geramirez03 <gabrieler2000@hotmail.com>
2023-12-18 15:31:31 -0800

    Implement Comment data model class

---
+++ backend/models/comment.js
@@ -0,0 +1,94 @@
+const db = require("../db");
+const { NotFoundError } = require("../expressError");
+const { sqlForPartialUpdate } = require("../helpers/sql");
+
+/** Related functions for comments. */
+
+class Comment {
+
+    /** Create a comment with data provided */
+
+    static async create({ author, review_id, text }) {
+
+        const result = await db.query(`
+            INSERT INTO comments
+            (author, review_id, text)
+            VALUES ($1, $2, $3)
+            RETURNING id, author, review_id, text, created_at`,
+            [author, review_id, text]
+        );
+        const comment = result.rows[0];
+
+        return comment;
+    }
+
+    /** Get a comment's information by id */
+
+    static async get(id) {
+
+        const result = await db.query(`
+            SELECT * 
+            FROM comments 
+            WHERE id = $1`, 
+            [id]
+        );
+        const comment = result.rows[0];
+
+        if (!comment) throw new NotFoundError(`Comment not found: ${id}`);
+
+        return comment;
+    }
+
+    /** Update a comment's text with `text` */
+
+    static async update(id, text) {
+
+        const result = await db.query(`
+            UPDATE comments
+            SET text = $1
+            WHERE id = $2
+            RETURNING id, author, text`,
+            [text, id]
+        );
+        const comment = result.rows[0];
+
+        if (!comment) throw new NotFoundError(`Comment not found: ${id}`);
+
+        return comment;
+    }
+
+    /** Update a comment's votes with `delta` (+1 or -1) */
+
+    static async vote(id, delta) {
+
+        const result = await db.query(`
+            UPDATE comments
+            SET votes = votes + $1
+            WHERE id = $2
+            RETURNING id, votes`,
+            [delta, id]
+        );
+        const comment = result.rows[0];
+
+        if (!comment) throw new NotFoundError(`Comment not found: ${id}`);
+
+        return comment;
+    }
+
+    static async remove(id) {
+
+        const result = db.query(`
+            DELETE FROM comments
+            WHERE id = $1
+            RETURNING id`,
+            [id]
+        );
+        const comment = result.rows[0];
+
+        if (!comment) throw new NotFoundError(`Comment not found: ${id}`);
+
+        return comment;
+    }
+}
+
+module.exports = Comment;
