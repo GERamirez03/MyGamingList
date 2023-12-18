d3fa3a7ea72b4ef026a550fe45b34639a6cd781b
geramirez03 <gabrieler2000@hotmail.com>
2023-12-18 13:01:20 -0800

    Implement updated_at functionality for reviews and comments with db trigger functionality; reorganize tables

--- backend/seed.sql
+++ backend/seed.sql
@@ -16,6 +16,7 @@ CREATE TABLE users
     is_admin BOOLEAN DEFAULT false
 );
 
+-- video game data from IGDB API
 CREATE TABLE games 
 (
     id INT PRIMARY KEY,
@@ -23,7 +24,8 @@ CREATE TABLE games
     slug TEXT NOT NULL,
     summary TEXT NOT NULL,
     first_release_date BIGINT NOT NULL,
-    cover_url TEXT DEFAULT NULL
+    cover_url TEXT DEFAULT NULL,
+    platforms TEXT NOT NULL
 );
 
 CREATE TABLE users_games
@@ -31,7 +33,19 @@ CREATE TABLE users_games
     id SERIAL PRIMARY KEY,
     user_id INT REFERENCES users (id),
     game_id INT REFERENCES games (id),
-    rating FLOAT DEFAULT 2.5
+    rating FLOAT DEFAULT 0 -- Each user_id's rating of game_id
+);
+
+-- Global ratings of games, averaged across all MGL users' individual ratings
+CREATE TABLE ratings
+(
+    game_id INT REFERENCES games (id),
+    rating FLOAT DEFAULT 0 
+    -- give users option to CRUD their ratings, reviews, feedback! 
+    -- NOTES: 
+    -- can use AVERAGE to help with computations
+    -- select sum(rating) from uses_games where game_id = 123
+    -- select rating from users_game where game_id = 123 and user_id = [logged in user]
 );
 
 CREATE TABLE reviews
@@ -43,6 +57,7 @@ CREATE TABLE reviews
     author TEXT REFERENCES users (username),
     game_id INT REFERENCES games(id),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     votes INT NOT NULL DEFAULT 0
 );
 
@@ -52,5 +67,17 @@ CREATE TABLE comments
     text TEXT NOT NULL,
     review_id INT REFERENCES reviews (id) ON DELETE CASCADE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     votes INT NOT NULL DEFAULT 0
 );
+
+CREATE OR REPLACE FUNCTION update_modified_column()
+RETURNS TRIGGER AS $$
+BEGIN
+NEW.updated_at = CURRENT_TIMESTAMP;
+RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+CREATE TRIGGER update_modified_column BEFORE UPDATE ON reviews FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
+CREATE TRIGGER update_modified_column BEFORE UPDATE ON comments FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
