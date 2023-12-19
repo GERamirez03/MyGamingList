197f73dd375eb2ffd54127d01b526d2a622399c1
geramirez03 <gabrieler2000@hotmail.com>
2023-12-19 13:53:16 -0800

    Reorganize Router routes and add tentative routes in comments

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -16,10 +16,12 @@ function Router() {
             <Route exact path="/signup" element={<SignupForm/>} />
             <Route exact path="/login" element={<LoginForm/>} />
             <Route exact path="/account" element={<AccountViewer/>} />
+            {/* <Route path="/reviews" element={<ReviewList ALL />} /> */}
             <Route exact path="/games/search" element={<GameList/>} />
+            <Route exact path="/reviews/new" element={<ReviewForm/>} />
             <Route path="/users/:username" element={<ProfileViewer/>} />
             <Route path="/games/:slug" element={<GameDetail/>} />
-            <Route path="/reviews/new" element={<ReviewForm/>} />
+            {/* <Route path="/reviews/:slug" element={<ReviewList SLUG />} /> */}
         </ Routes>
     );
 }
