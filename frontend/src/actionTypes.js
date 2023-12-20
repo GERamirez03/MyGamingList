7b4592fdcec6dc1fd4f4bc348a8ecdf520647828
geramirez03 <gabrieler2000@hotmail.com>
2023-12-19 18:46:30 -0800

    Define REMOVE_REVIEW action type

--- frontend/src/actionTypes.js
+++ frontend/src/actionTypes.js
@@ -5,3 +5,4 @@ export const ADD_GAME = "ADD_GAME";
 export const REMOVE_GAME = "REMOVE_GAME";
 export const RATE_GAME = "RATE_GAME";
 export const ADD_REVIEW = "ADD_REVIEW";
+export const REMOVE_REVIEW = "REMOVE_REVIEW";
