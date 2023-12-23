3dad0d9ad6b97e5bb20c3b3fa3a5b34387770823
geramirez03 <gabrieler2000@hotmail.com>
2023-12-22 17:06:25 -0800

    Refactor CommentSection to pass necessary props to CommentForm

--- frontend/src/CommentSection.js
+++ frontend/src/CommentSection.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Box, Typography } from "@mui/material";
 import CommentCard from "./CommentCard";
 import CommentForm from "./CommentForm";
+import UserContext from "./userContext";
+import { useDispatch } from "react-redux";
+import { sendUserPostingCommentToApi } from "./actionCreators";
+
 
 function CommentSection({ comments, reviewId }) {
     const isEmpty = comments.length === 0;
+
+    const apiHelper = useContext(UserContext);
+    const dispatch = useDispatch();
+
+    const postComment = commentFormData => {
+        dispatch(sendUserPostingCommentToApi(commentFormData, apiHelper));
+    }
+
     return (
         <Box>
-            <CommentForm reviewId={reviewId} />
+            <CommentForm postComment={postComment} reviewId={reviewId} author={apiHelper.username} />
             {
                 isEmpty
                 ? <Typography variant="p">Be the first to comment!</Typography>
