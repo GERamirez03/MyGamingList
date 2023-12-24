8f88bff599dfcd3c2da85520479e2084f6fdc183
geramirez03 <gabrieler2000@hotmail.com>
2023-12-23 18:05:01 -0800

    Define comment action types: ADD, REMOVE, and UPDATE

--- frontend/src/actionTypes.js
+++ frontend/src/actionTypes.js
@@ -8,3 +8,5 @@ export const ADD_REVIEW = "ADD_REVIEW";
 export const REMOVE_REVIEW = "REMOVE_REVIEW";
 export const UPDATE_REVIEW = "UPDATE_REVIEW";
 export const ADD_COMMENT = "ADD_COMMENT";
+export const REMOVE_COMMENT = "REMOVE_COMMENT";
+export const UPDATE_COMMENT = "UPDATE_COMMENT";
